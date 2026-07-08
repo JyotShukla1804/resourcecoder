9331a4e6d4ba7e7dfac4ba90ca07a7f183680438
JyotShukla1804 <jyot.krazio@gmail.com>
2026-07-08 15:40:24 +0530

    errorfix

--- fix_svg.ts
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'public/metaverse-standards-logo-60px.svg';
-let data = fs.readFileSync(file, 'utf8');
-data = data.replace(/style="fill-rule:nonzero;"/g, 'style="fill:#ffffff;fill-rule:nonzero;"');
-fs.writeFileSync(file, data);
-console.log('SVG updated successfully');
